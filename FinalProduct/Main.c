e72f4197301bebbfab7b9dc9f4cefc02da60bfc4
Goat King <joaki2003@gmail.com>
2024-12-17 14:54:48 +0100

    more stuf

--- FinalProduct/Main.c
+++ FinalProduct/Main.c
@@ -1,4 +1,5 @@
 #include "functions.h"
+#include <stdio.h>
 
 typedef struct pref pref;
 typedef struct conpref conpref;
@@ -8,6 +9,7 @@ int main(void){
     pref newMovie;
     conpref conWatch;
     inputMain(&nc, &newMovie, &conWatch);
+    printf("New movie data. Genre1: %s, genre2: %s, genre3: %s, actor1: %s, actor2: %s, actor3: %s, time to watch: %i", newMovie.genre1, newMovie.genre2, newMovie.genre3, newMovie.actor1, newMovie.actor2, newMovie.actor3, newMovie.timetowatch);
     movieSortMain(nc, &newMovie, &conWatch);
 
     return 0;
