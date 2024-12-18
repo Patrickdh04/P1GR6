878c186221675e513742b51c43ff89e0691c9aac
Askernl <asker.nl@hotmail.com>
2024-12-18 16:58:28 +0100

    did some cool stuff with mac os

--- FinalProduct/timeLimitSetting.c
+++ FinalProduct/timeLimitSetting.c
@@ -5,6 +5,7 @@
 
 void setTimeSetting(int *timeSettingChosen)
 {
+   #ifdef _WIN32
     const char *filename = "Warning.vbs";
 
     // Try to open the file in read mode
@@ -26,23 +27,26 @@ void setTimeSetting(int *timeSettingChosen)
         fprintf(file, "x=msgbox(\"You have reached your watch-limit\",0, \"REMINDER!\")"); // the command going in the file
         fclose(file);
     }
+    #elif __APPLE__
+       FILE *File = fopen("warning.txt", "w");
+        if (File == NULL)
+        {
+            perror("Fejl ved åbning af output-fil");
+            return;
+        }
+         fprintf(File, "Your time is up!!!!");
+         
+    #endif
 
     printf("You can choose 3 different types of interventions for your binge-session.\n"
            "Choose what will happen when you have reached your watching limit:\n\n"
            "Press 1 to get a pop-up message.\n"
            "Press 2 to close your streaming service window.\n"
            "Press 3 to shut down your computer 5 minutes after your time is up.\n");
-
     while (*timeSettingChosen != 1 && *timeSettingChosen != 2 && *timeSettingChosen != 3)
     {
         scanf(" %d", timeSettingChosen);
-
-        getchar(); // Clears the leftover '\n', but can also use %[^\n]
-        fgets(*timeSettingChosen, MAX_LENGTH, stdin);
-        fprintf(file, "%s", *timeSettingChosen);
-        fclose(file);
     }
-
 }
 
 void startWarning(int choice, char nc, int newMovieTime, int conWatchTime)
@@ -66,20 +70,23 @@ void startWarning(int choice, char nc, int newMovieTime, int conWatchTime)
     switch (choice)
     {
     case 1:
-        #ifdef _WIN32
+#ifdef _WIN32
+        
         system("Warning.vbs");
-        #endif
+#elif __APPLE__
+        system("open warning.txt");
+#endif
         break;
     case 2:
         printf("\nClosing streaming service...\n");
-        #ifdef _WIN32
+#ifdef _WIN32
         system("Warning.vbs");
         system("C:\\WINDOWS\\System32\\taskkill /F /T /IM chrome.exe > nul 2>&1");
         system("C:\\WINDOWS\\System32\\taskkill /F /T /IM msedge.exe > nul 2>&1");
-        #elif __APPLE__ 
+#elif __APPLE__
+        system("open warning.txt");
         system("pkill Safari");
-        #endif
-
+#endif
 
         break;
     case 3:
